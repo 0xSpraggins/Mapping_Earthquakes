4b564d85eb227d9c216e576b7af3789dd9a6e7f1
baileyspraggins <62624378+baileyspraggins@users.noreply.github.com>
2020-06-27 23:43:39 -0500

    Dashed Airline Map

--- Mapping_Lines/static/js/logic.js
+++ Mapping_Lines/static/js/logic.js
@@ -1,23 +1,31 @@
 // Add console.log to check to see if our code is working.
 console.log("working");
 
-// Create the map object with a center and zoom level.
-let map = L.map("mapid").setView([40.7, -94.5], 4);
+// Create the map object with center at the San Francisco airport.
+let map = L.map('mapid').setView([37.6213, -122.3790], 5);
 
-// Get data from cities.js
-let cityData = cities;
+// Coordinates for each point to be used in the polyline.
+let line = [
+	// San Francisco Airport
+	[37.6213, -122.3790],
+	// Austin-Bergstrom Airport
+	[30.1975, -97.6664],
+	// Chicago O'Hare Airport
+	[41.9742, -87.9073],
+	// Toronto Pearson Airport
+	[43.6777, -79.6248],
+	// John F. Kennedy Airport
+	[40.6413, -73.7781],
 
-// Loop through the cities array and create one marker for each city.
-cityData.forEach(function(city) {
-	console.log(city)
-	L.circleMarker(city.location, {
-    radius: city.population / 100000,
-    color: "black",
-    fillColor: "#ffffa1" 
-  })
-	.bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population: " + city.population.toLocaleString() + "</h3>")
-  .addTo(map);
-});
+  ];
+
+// Create a polyline using the line coordinates and make the line black.
+L.polyline(line, {
+	color: "Blue",
+	dashArray: "5,10",
+	opacity: .5,
+	weight: 4,
+ }).addTo(map);
 
 // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
